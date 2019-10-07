ab94d84db1d60334dbd97c81867e1fb1a3532af2
Iago <yagomello16@gmail.com>
2019-10-07 15:43:45 -0300

    initial structure

---
+++ src/components/Form.vue
@@ -0,0 +1,40 @@
+<template>
+  <b-form-row class="justify-content-center w-50 mx-auto">
+    <b-col cols="12">
+      <b-form-group label="Name" label-for="name">
+        <b-form-input v-model="$v.model.name.$model"></b-form-input>
+        <div v-if="$v.model.name.$error">
+          <span v-if="!$v.model.name.$required" style="color: red;">Name is required</span>
+        </div>
+      </b-form-group>
+    </b-col>
+
+    <b-col cols="12" sm="4" md="3" lg="2">
+      <b-btn variant="primary" block @click="$v.$touch()">Submit</b-btn>
+    </b-col>
+    <b-col cols="12" sm="4" md="3" lg="2">
+      <b-btn variant="outline-danger" block @click="$v.$reset()">Reset</b-btn>
+    </b-col>
+  </b-form-row>
+</template>
+
+<script>
+import { required } from 'vuelidate/lib/validators'
+export default {
+  name: 'Form',
+  validations: {
+    model: {
+      name: { required }
+    }
+  },
+  data() {
+    return {
+      model: {
+        name: ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss"></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <b-container>
+    <Form msg="Welcome to Your Vue.js App" />
+  </b-container>
+</template>
+
+<script>
+// @ is an alias to /src
+import Form from '@/components/Form.vue'
+
+export default {
+  name: 'home',
+  components: { Form }
+}
+</script>
